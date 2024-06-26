1b330bb8a0b6ccc74dd5318ad424f9655d3b6409
Teo Sibileau <teo.sibileau@gmail.com>
2024-06-26 15:47:42 -0300

    Extract URL path

--- app/main.py
+++ app/main.py
@@ -1,17 +1,27 @@
 
 import socket
 
+HTTP_200 = b"HTTP/1.1 200 OK\r\n\r\n"
+HTTP_404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
 def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
     while True:
-        client_socket, client_address = server_socket.accept()  # wait for client
-        print(f"client Address: {client_address}")
-        response = b"HTTP/1.1 200 OK\r\n\r\n"
-        client_socket.sendall(response)
-        client_socket.close()
+        connection, address = server_socket.accept()
+        print(f"client Address: {address}")
+        data = connection.recv(1024)
+        request_data = data.decode("utf-8").split("\r\n")
+        if not data:
+            break
+        path = request_data[0].split(" ")[1]
+        if path == "/":
+            connection.sendall(HTTP_200)
+        else:
+            connection.sendall(HTTP_404)
+        connection.close()
 
 
 if __name__ == "__main__":
