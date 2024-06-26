f7c5ee112ee00812dfcd4da4469bc03bb74e5e62
Teo Sibileau <teo.sibileau@gmail.com>
2024-06-26 15:25:38 -0300

    Respond with 200

--- app/main.py
+++ app/main.py
@@ -6,7 +6,12 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    while True:
+        client_socket, client_address = server_socket.accept()  # wait for client
+        print(f"client Address: {client_address}")
+        response = b"HTTP/1.1 200 OK\r\n\r\n"
+        client_socket.sendall(response)
+        client_socket.close()
 
 
 if __name__ == "__main__":
