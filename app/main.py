dd940ccfd2d345c7747a62c86fa66c40b33b9c53
Teofilo Sibileau <teo.sibileau@gmail.com>
2026-06-26 16:36:22 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-
 import socket
 import threading
 import argparse
@@ -6,11 +5,14 @@ import os
 
 
 class Request:
-    def __init__(self, data: bytes):
-        data = data.decode("utf-8").split("\r\n")
+    def __init__(self, raw_data: bytes):
+        parts = raw_data.decode("utf-8").split("\r\n\r\n")
+        self.body = None
+        if len(parts) > 1:
+            self.body = parts[1]
+        data = parts[0].split("\r\n")
         self.method, self.path, self.protocol = data[0].split(" ")
-        self.headers = {
-        }
+        self.headers = {}
         for header in data[1:]:
             if not header:
                 continue
@@ -23,6 +25,7 @@ class Response:
     def __init__(self, status_code=200, body="", content_type="text/plain"):
         codes = {
             200: "OK",
+            201: "Created",
             404: "Not Found",
         }
         self.status = f"{status_code} {codes[status_code]}"
@@ -59,18 +62,28 @@ def handle_client(connection, address, cli_args):
     elif request.path.startswith("/echo/"):
         response = Response(body=request.path.split("/echo/")[1])
     elif request.path.startswith("/files/"):
-        files = os.listdir(cli_args.directory)
-        file_to_retrieve = request.path.split("/files/")[1]
-        if file_to_retrieve in files:
-            file_path = cli_args.directory + file_to_retrieve
-            with open(file_path, "r") as file:
-                response = Response(body=file.read(), content_type="application/octet-stream")
-        else:
-            response = Response(status_code=404, body="Not found")
+        if request.method == "GET":
+            files = os.listdir(cli_args.directory)
+            file_to_retrieve = request.path.split("/files/")[1]
+            if file_to_retrieve in files:
+                file_path = cli_args.directory + file_to_retrieve
+                with open(file_path, "r") as file:
+                    response = Response(
+                        body=file.read(), content_type="application/octet-stream"
+                    )
+            else:
+                response = Response(status_code=404, body="Not found")
+        elif request.method == "POST":
+            file_to_save = request.path.split("/files/")[1]
+            file_path = cli_args.directory + file_to_save
+            with open(file_path, "w+") as file:
+                file.write(request.body)
+            response = Response(body="File saved", status_code=201)
     else:
         response = Response(status_code=404, body="Not found")
     connection.sendall(bytes(response))
     connection.close()
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -84,7 +97,9 @@ def main():
     try:
         while True:
             connection, address = server_socket.accept()
-            threading.Thread(target=handle_client, args=(connection, address, cli_args)).start()
+            threading.Thread(
+                target=handle_client, args=(connection, address, cli_args)
+            ).start()
     except KeyboardInterrupt:
         print("\nServer is shutting down.")
     finally:
